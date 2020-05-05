0bb5e7ae3acac37a234d3c010ae97629c067a6ff
sungraizfaryad <sungraiz@gmail.com>
2020-05-05 23:53:33 +0500

    version changed

--- remove-taxonomy-url.php
+++ remove-taxonomy-url.php
@@ -33,7 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'REMOVE_TAXONOMY_URL_VERSION', '1.0.0' );
+define( 'REMOVE_TAXONOMY_URL_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
